5b114b354db7a05de0ef159f14f0a9517b5c4469
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-06-24 21:55:22 -0700

    fail length is 100

--- test_fizzbuzz_tdd.rb
+++ test_fizzbuzz_tdd.rb
@@ -11,4 +11,10 @@ class TestTddFizzBuzz < Minitest::Test
     	assert_equal(Array, fizzbuzz().class)
     end
 
+    def test_assert_that_length_is_100
+    	assert_equal(100, fizzbuzz().length)
+    end
+    # def test_assert_that_first_position_is_1
+    # 	assert_equal(1, fizzarr()[0])
+    # end
 end
